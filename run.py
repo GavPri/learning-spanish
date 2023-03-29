09a6f3dbaa77b0071cb0a7aa35e331938de83ae6
GavPri <gavinprior5@gmail.com>
2023-03-29 18:30:30 +0000

    Wired up application to google sheets

--- run.py
+++ run.py
@@ -1,1 +1,22 @@
 # python code goes here
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('new_phrases')
+
+
+en_es = SHEET.worksheet('english_to_spanish')
+en_es_data = en_es.get_all_values()
+
+
+print(en_es_data)
