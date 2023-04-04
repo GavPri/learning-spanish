48dc476a00b7381de73865162beba346b9a828cc
GavPri <gavinprior5@gmail.com>
2023-04-04 11:41:59 +0000

    refactoring of translation functions

--- run.py
+++ run.py
@@ -50,65 +50,89 @@ def lang_choice_beta():
         return lang_choice_beta()
 
 
-def trans_english_to_spanish():
-    """
-    Function to translate user inout from English To Spanish. 
-    """
+# def trans_english_to_spanish():
+#     """
+#     Function to translate user inout from English To Spanish. 
+#     """
+#     while True:
+#         global phrase
+#         translator = Translator()
+#         phrase = input(
+#             'Please enter the phrase that you would like to be translated: '
+#         )
+#         if phrase.isdigit():
+#             print('Please enter words and phrases, not digits!\n')
+#             continue
+#         else:
+#             break
+
+#     language_en = translator.detect(phrase).lang
+
+#     if language_en != 'en':
+#         print('Oops, no language was detected. Please try again')
+#         return trans_english_to_spanish()
+#     else:
+#         translation = translator.translate(phrase, src='en', dest='es')
+#         print(f' {phrase} is being translated to Spanish...\n')
+#         print(f'"{phrase}" translates to "{translation.text}" in Spanish\n')
+#         worksheet = 'english_to_spanish'
+#         update_worksheet(phrase, translation, worksheet)
+#         return lang_choice()
+
+
+# def trans_spanish_to_english():
+#     """
+#     Function to translate user input from Spanish to English.
+#     """
+#     global phrase
+#     while True:
+#         translator = Translator()
+#         phrase = input(
+#             'Please enter the phrase that you would like to be translated: '
+#         )
+
+#         if phrase.isdigit():
+#             print('Please enter words and phrases, not digits!\n')
+#             continue
+#         else:
+#             break
+
+#     language = translator.detect(phrase).lang
+
+#     if language != 'es':
+#         print('Oops, Spanish was not detected, please try again!')
+#         return trans_spanish_to_english()
+#     else:
+#         translation = translator.translate(phrase, src='es', dest='en')
+#         print(f'\n {phrase} is being translated to English..\n')
+#         print(f'"{phrase}" translates to "{translation.text}" in English\n')
+#         worksheet = 'spanish_to_english'
+#         update_worksheet(phrase, translation, worksheet)
+#         return lang_choice()
+
+
+def translate_phrase(src_lang, dest_lang):
     while True:
-        global phrase
         translator = Translator()
-        phrase = input(
-            'Please enter the phrase that you would like to be translated: '
-        )
-        if phrase.isdigit():
-            print('Please enter words and phrases, not digits!\n')
-            continue
-        else:
-            break
-
-    language_en = translator.detect(phrase).lang
-
-    if language_en != 'en':
-        print('Oops, no language was detected. Please try again')
-        return trans_english_to_spanish()
-    else:
-        translation = translator.translate(phrase, src='en', dest='es')
-        print(f' {phrase} is being translated to Spanish...\n')
-        print(f'"{phrase}" translates to "{translation.text}" in Spanish\n')
-        worksheet = 'english_to_spanish'
-        update_worksheet(phrase, translation, worksheet)
-        return lang_choice()
-
-
-def trans_spanish_to_english():
-    """
-    Function to translate user input from Spanish to English.
-    """
-    global phrase
-    while True:
-        translator = Translator()
-        phrase = input(
-            'Please enter the phrase that you would like to be translated: '
-        )
+        phrase = input('Please enter the phrase you would like to have translated: ')
 
         if phrase.isdigit():
-            print('Please enter words and phrases, not digits!\n')
+            print('Please only enter words and phrases, not digits!')
             continue
         else:
             break
 
     language = translator.detect(phrase).lang
 
-    if language != 'es':
-        print('Oops, Spanish was not detected, please try again!')
-        return trans_spanish_to_english()
+    if language != src_lang:
+            print(f'Oops! {src_lang} was not detected, please try again.')
     else:
-        translation = translator.translate(phrase, src='es', dest='en')
-        print(f'\n {phrase} is being translated to English..\n')
-        print(f'"{phrase}" translates to "{translation.text}" in English\n')
-        worksheet = 'spanish_to_english'
-        update_worksheet(phrase, translation, worksheet)
-        return lang_choice()
+        translation = translator.translate(phrase, src=src_lang, dest=dest_lang)
+        print(f'\n {phrase} is being translated to {dest_lang}..\n')
+        print(f'"{phrase}" translates to "{translation.text}" in {dest_lang}\n')
+        worksheet_name = f'{src_lang}_to_{dest_lang}'
+        update_worksheet(phrase, translation, worksheet_name, src_lang, dest_lang)
+        lang_choice_beta()
 
 
 def update_worksheet(phrase, translation, worksheet):
